84861122ed9e8249c7ead8e6330be63da1fb73c1
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-01-20 00:03:03 -0500

    Create method of setting up players

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,19 @@
 require_relative 'lib/player'
 
-test_player = Player.new('Berry', 'X')
-p test_player.name
-p test_player.symbol
+def setup_players
+  puts 'Who is playing as X?'
+  name = gets.chomp
+  playerX = Player.new(name, 'X')
+  
+  puts # for a line break
+  
+  puts 'Who is playing as O?'
+  name = gets.chomp
+  playerO = Player.new(name, 'O')
+  
+  puts
+  
+  puts "Your players are...
+  #{playerX.name} as #{playerX.symbol}
+  #{playerO.name} as #{playerO.symbol}"
+end
