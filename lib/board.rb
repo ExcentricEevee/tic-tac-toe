956875e98f49ca4b2f709933ac720fceeb16acbe
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-01-21 23:32:04 -0500

    Add method to show current board state

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,22 @@
 # Class that tracks the state of the game area;
 # what symbols are placed in it, win condition checking, etc
 class Board
-  attr_accessor :board
+  attr_accessor :squares
 
   def initialize
     # 3 arrays of size-3? 1 array of size-9?
-    @board = Array.new(9, " ")
+    @squares = Array.new(9, " ")
+  end
+
+  def show
+    puts <<-HEREDOC
+
+     #{squares[0]} | #{squares[1]} | #{squares[2]}
+    ---+---+---
+     #{squares[3]} | #{squares[4]} | #{squares[5]}
+    ---+---+---
+     #{squares[6]} | #{squares[7]} | #{squares[8]}
+
+    HEREDOC
   end
 end
